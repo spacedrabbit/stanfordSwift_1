27fd35534a4336edf27d16cb3f81bc0e4a3313aa
Louis Tur <louis.tur@gmail.com>
2015-08-28 22:29:44 -0400

    multiply operation in place using functions as parameters

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -10,6 +10,71 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var display: UILabel!
+    var userIsTypingANumber: Bool = false
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        println("digit = \(digit)")
+        
+        if userIsTypingANumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsTypingANumber = true
+        }
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        
+        let operation = sender.currentTitle!
+        if userIsTypingANumber{
+            enterKey()
+        }
+        
+        switch operation {
+        case "×": performOperation(multiply)
+            
+//        case "÷": performOperation()
+//            
+//        case "+":
+//            
+//        case "−":
+        
+        default:
+            println("Stuff")
+        }
+    }
+    
+    func performOperation(operation: (Double, Double) -> Double){
+        if operandStack.count >= 2{
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enterKey()
+        }
+    }
+    
+    func multiply(opt1: Double, opt2: Double) -> Double{
+        return opt1 * opt2
+    }
+    
+    var operandStack = Array<Double>()
+    
+    @IBAction func enterKey() {
+        userIsTypingANumber = false
+        operandStack.append(displayValue)
+        println("operand stack = \(operandStack)")
+    }
+    
+    // initializer class
+    var displayValue: Double{
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsTypingANumber = false
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
