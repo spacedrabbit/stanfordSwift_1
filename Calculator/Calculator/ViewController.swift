dae34fd3e840edfcd8cdac5a8d18aa0922c0b120
Louis Tur <louis.tur@gmail.com>
2015-08-30 23:59:10 -0400

    removes extended explanation of closures

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -46,6 +46,8 @@ class ViewController : UIViewController {
         switch symbol {
         case "π":
             value = M_PI
+        case "e":
+            value = M_E
         default:
             value = 0.0
         }
@@ -55,60 +57,6 @@ class ViewController : UIViewController {
     }
     
     @IBAction func operate(sender: UIButton) {
-        
-        /*
-        it is possible to send functions as arguments to other funcitons
-        the receiving function just has to specify what kind of function
-        it allows, much like passing blocks in ObjC
-        case "×": performOperation(multiply)
-        
-        additionally, you can just write the function inline with the call
-        using closures. In this case, it is essentially a copy/paste
-        of the function's signature with some slight alterations
-        
-        func multiply(opt1: Double, opt2: Double) -> Double{
-        return opt1 * opt2
-        }
-        
-        - take everything after the function's name
-        (opt1: Double, opt2: Double) -> Double{
-        return opt1 * opt2
-        }
-        
-        - move the opening '{' and put it at the very front
-        and add 'in' where it used to be
-        { (opt1: Double, opt2: Double) -> Double in
-        return opt1 * opt2
-        }
-        
-        - this gets us to this:
-        performOperation({(opt1: Double, opt2: Double) -> Double in
-        return opt1 * opt2
-        })
-        
-        - But, since Swift is really good at type inference, we dont
-        need to specify the types we pass into our function
-        performOperation({ (opt1, opt2) in return opt1 * opt2 })
-        
-        - If our whole implementation is just an expression, the return
-        keyword isn't needed either. Swift also already knows that
-        performOperation returns something
-        performOperation({ (opt1, opt2) in opt1 * opt2 })
-        
-        - Further, you dont need to name your variables passed. If you
-        dont name them, swift refers to them as $0 and $1, etc.
-        performOperation({ $0 * $1 })
-        
-        - Now, if a function receives another function as an argument AND
-        that is the last parameter -- meaning there could be other arugments
-        needed, but they come before the function argument -- then you
-        can write the expression outside of the parenthesis
-        performOperation() { $0 * $1 }
-        
-        - Lastly, if a function is the only argument passed, you can drop the ()
-        performOperation { $0 * $1 }
-        */
-        
         
         let operation = sender.currentTitle!
         if userIsTypingANumber{
