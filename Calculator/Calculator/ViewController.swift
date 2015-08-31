158aeac74f0f578566d5608908902a3dbb55e97b
Louis Tur <louis.tur@gmail.com>
2015-08-30 23:48:53 -0400

    Adds additional buttons. Pi, cos, and sin operations are working as intended

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -16,20 +16,99 @@ class ViewController : UIViewController {
     // this action handles all digit presses
     @IBAction func appendDigit(sender: UIButton) {
         
-        // a UIButton's currentTitle property is normally an optional
-        // we explicitly unwrap it to be used for our constant here
-        let digit = sender.currentTitle!
+        var digit = sender.currentTitle!
         println("digit = \(digit)")
-        
+
         if userIsTypingANumber {
             display.text = display.text! + digit
         } else {
+            if digit == "π" {
+                digit = "\(M_PI)"
+                enterKey()
+            } else {
+                userIsTypingANumber = true
+            }
             display.text = digit
-            userIsTypingANumber = true
         }
     }
     
+    @IBAction func appendSymbol(sender: UIButton) {
+        
+        var symbol = sender.currentTitle!
+        var value: Double
+        var numericString: String
+        println("symbol \(symbol)")
+        
+        if userIsTypingANumber {
+            enterKey()
+        }
+        
+        switch symbol {
+        case "π":
+            value = M_PI
+        default:
+            value = 0.0
+        }
+        
+        display.text = "\(value)"
+        enterKey()
+    }
+    
     @IBAction func operate(sender: UIButton) {
+        
+        /*
+        it is possible to send functions as arguments to other funcitons
+        the receiving function just has to specify what kind of function
+        it allows, much like passing blocks in ObjC
+        case "×": performOperation(multiply)
+        
+        additionally, you can just write the function inline with the call
+        using closures. In this case, it is essentially a copy/paste
+        of the function's signature with some slight alterations
+        
+        func multiply(opt1: Double, opt2: Double) -> Double{
+        return opt1 * opt2
+        }
+        
+        - take everything after the function's name
+        (opt1: Double, opt2: Double) -> Double{
+        return opt1 * opt2
+        }
+        
+        - move the opening '{' and put it at the very front
+        and add 'in' where it used to be
+        { (opt1: Double, opt2: Double) -> Double in
+        return opt1 * opt2
+        }
+        
+        - this gets us to this:
+        performOperation({(opt1: Double, opt2: Double) -> Double in
+        return opt1 * opt2
+        })
+        
+        - But, since Swift is really good at type inference, we dont
+        need to specify the types we pass into our function
+        performOperation({ (opt1, opt2) in return opt1 * opt2 })
+        
+        - If our whole implementation is just an expression, the return
+        keyword isn't needed either. Swift also already knows that
+        performOperation returns something
+        performOperation({ (opt1, opt2) in opt1 * opt2 })
+        
+        - Further, you dont need to name your variables passed. If you
+        dont name them, swift refers to them as $0 and $1, etc.
+        performOperation({ $0 * $1 })
+        
+        - Now, if a function receives another function as an argument AND
+        that is the last parameter -- meaning there could be other arugments
+        needed, but they come before the function argument -- then you
+        can write the expression outside of the parenthesis
+        performOperation() { $0 * $1 }
+        
+        - Lastly, if a function is the only argument passed, you can drop the ()
+        performOperation { $0 * $1 }
+        */
+        
         
         let operation = sender.currentTitle!
         if userIsTypingANumber{
@@ -38,63 +117,15 @@ class ViewController : UIViewController {
         
         switch operation {
             
-            // it is possible to send functions as arguments to other funcitons
-            // the receiving function just has to specify what kind of function
-            // it allows, much like passing blocks in ObjC
-        // case "×": performOperation(multiply)
-            
-            // additionally, you can just write the function inline with the call
-            // using closures. In this case, it is essentially a copy/paste
-            // of the function's signature with some slight alterations
-            /*
-                func multiply(opt1: Double, opt2: Double) -> Double{
-                    return opt1 * opt2
-                }
-                
-                // take everything after the function's name
-                (opt1: Double, opt2: Double) -> Double{
-                    return opt1 * opt2
-                }
-            
-                // move the opening '{' and put it at the very front
-                // and add 'in' where it used to be
-                { (opt1: Double, opt2: Double) -> Double in
-                    return opt1 * opt2
-                }
-            
-                // this gets us to this:
-                performOperation({(opt1: Double, opt2: Double) -> Double in
-                    return opt1 * opt2
-                })
-            
-                // But, since Swift is really good at type inference, we dont
-                // need to specify the types we pass into our function
-                performOperation({ (opt1, opt2) in return opt1 * opt2 })
-            
-                // If our whole implementation is just an expression, the return
-                // keyword isn't needed either. Swift also already knows that
-                // performOperation returns something
-                performOperation({ (opt1, opt2) in opt1 * opt2 })
-            
-                // Further, you dont need to name your variables passed. If you
-                // dont name them, swift refers to them as $0 and $1, etc.
-                performOperation({ $0 * $1 })
-            
-                // Now, if a function receives another function as an argument AND
-                // that is the last parameter -- meaning there could be other arugments
-                // needed, but they come before the function argument -- then you 
-                // can write the expression outside of the parenthesis
-                performOperation() { $0 * $1 }
-            
-                // Lastly, if a function is the only argument passed, you can drop the ()
-                performOperation { $0 * $1 }
-            */
-            
         case "×": performOperation { $0 * $1 }
         case "÷": performOperation { $1 / $0 }
         case "+": performOperation { $0 + $1 }
         case "−": performOperation { $1 - $0 }
         case "√": performOperation { sqrt($0) }
+            
+        // these two math() functions are explained in BSD man pages
+        case "cos": performOperation { __cospi($0 / 180) }
+        case "sin": performOperation { __sinpi($0 / 180) }
             
         default:
             println("Stuff")
@@ -116,13 +147,7 @@ class ViewController : UIViewController {
             enterKey()
         }
     }
-
-    // because swift is strongly typed, it can perform many type inferences 
-    // in this case, our variable declaration could be written as:
-    //
-    // var operandStack: Array<Double> = Array<Double>()
-    //
-    // but we can omit the Array<T> before the equals because of that inference
+    
     var operandStack = [Double]()
     
     @IBAction func enterKey() {
@@ -142,6 +167,8 @@ class ViewController : UIViewController {
             userIsTypingANumber = false
         }
     }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
